0f15ca0e0f046b76e2bb839ddf58d5cb7a8be5f2
oabmorgan <38126042+oabmorgan@users.noreply.github.com>
2018-11-02 22:14:27 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,7 @@
 var filter = window.location.search.substring(1);
-var allImages = document.getElementsByTagName('img');
-
-runfilter()
-
-function runfilter(){
-    if(filter.length > 0){
-        for(var i = 0; i < allImages.length ; i++) {
-            if(filter == allImages[i].id){
-                allImages[i].parentNode.parentNode.parentNode.style.display = 'none';
-            }
-        }
+var images = document.getElementsByTagName('img');
+for(var i = 0; i < images.length; i++) {
+    if(images[i].id == filter){
+        images[i].parentElement.parentElement.parentElement.style.display = 'none';
     }
-    document.getElementById('filter').innerHTML = "-"+filter;
 }
