17bf7de1f5875aacbfe4f011028bd55548570ade
oabmorgan <38126042+oabmorgan@users.noreply.github.com>
2018-11-02 22:55:45 +0900

    filter

--- scripts.js
+++ scripts.js
@@ -1,7 +1,17 @@
 var filter = window.location.search.substring(1);
-var images = document.getElementsByTagName('img');
-for(var i = 0; i < images.length; i++) {
-    if(images[i].id == filter){
-        images[i].parentElement.parentElement.parentElement.style.display = 'none';
+var icons = document.getElementsByClassName("icons");
+
+if(filter.length > 0){
+    for(var i = 0; i < icons.length; i++) {
+        var imgs = icons[i].getElementsByTagName('img');
+        var hide = true;
+        for(var j=0; j<imgs.length; j++){
+            if(imgs[j].id == filter){
+                hide=false;
+            }
+        }
+        if(hide){
+            icons[i].parentElement.parentElement.style.display = 'none';
+        }
     }
 }
