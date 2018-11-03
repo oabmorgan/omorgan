a38045676f2ec8ff59ded635ff6262e5ea5039c7
oabmorgan <38126042+oabmorgan@users.noreply.github.com>
2018-11-03 10:45:14 +0900

    contact

--- scripts.js
+++ scripts.js
@@ -1,17 +1,39 @@
-var filter = window.location.search.substring(1);
-var icons = document.getElementsByClassName("icons");
+runFilter(window.location.search.substring(1));
 
-if(filter.length > 0){
+function runFilter(filter){
+    var icons = document.getElementsByClassName("icons");
     for(var i = 0; i < icons.length; i++) {
-        var imgs = icons[i].getElementsByTagName('img');
-        var hide = true;
-        for(var j=0; j<imgs.length; j++){
-            if(imgs[j].id == filter){
-                hide=false;
+            var imgs = icons[i].getElementsByTagName('img');
+            var hide = filter;
+            for(var j=0; j<imgs.length && hide; j++){
+                if(imgs[j].id == filter){
+                    hide=false;
+                }
+            }
+            if(hide){
+                icons[i].parentElement.parentElement.style.display = 'none';
+            } else{
+                icons[i].parentElement.parentElement.style.display = 'block';
             }
         }
-        if(hide){
-            icons[i].parentElement.parentElement.style.display = 'none';
-        }
+    if(!filter){
+        document.getElementById("nofilter").style.color = '#fff';
+    } else{
+        document.getElementById("nofilter").style.color = '#666';
+    }
+    if(filter == "art"){
+        document.getElementById("artfilter").style.color = '#fff';
+    } else{
+        document.getElementById("artfilter").style.color = '#666';
+    }
+    if(filter == "programming"){
+        document.getElementById("progfilter").style.color = '#fff';
+    } else{
+        document.getElementById("progfilter").style.color = '#666';
+    }
+    if(filter == "game"){
+        document.getElementById("gamefilter").style.color = '#fff';
+    } else{
+        document.getElementById("gamefilter").style.color = '#666';
     }
 }
